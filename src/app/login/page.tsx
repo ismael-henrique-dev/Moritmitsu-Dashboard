6736057261f9a597fea2d6065ff3ed249f5eb398
Ismael <ismael.henrique.dev@gmail.com>
2025-10-17 17:20:26 -0300

    refactor(login): adjustement login form style and login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,6 @@
 import { LoginForm } from '@/components/forms/login-form'
 import { Metadata } from 'next'
 import Image from 'next/image'
-import Link from 'next/link'
 
 export const metadata: Metadata = {
   title: 'Login',
@@ -10,39 +9,25 @@ export const metadata: Metadata = {
 export default function LoginPage() {
   return (
     <div className='grid min-h-svh lg:grid-cols-2'>
-      <div className='bg-red-700/90 relative hidden w-full h-full lg:flex items-center justify-center'>
+      <div className='bg-morimitsu-red relative hidden w-full h-full lg:flex items-center justify-center'>
         <Image
           src='/logo_morimtsu.png'
-          alt='Image'
-          className='h-80 w-80 object-cover dark:brightness-[0.2] dark:grayscale'
-          width={400}
-          height={400}
+          alt='Morimtisu Logo'
+          className='h-64 w-64 object-cover dark:brightness-[0.2] dark:grayscale'
+          width={256}
+          height={256}
         />
       </div>
       <div className='flex flex-col gap-4 p-6 md:p-10'>
-        {/* <div className='flex justify-center gap-2 md:justify-start'>
-          <Link href='/login' className='flex items-center gap-2 font-medium'>
-            <div className='bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-full'>
-              <Image
-                src='/logo_morimtsu.png'
-                alt='Image'
-                className='h-6 w-6 object-cover dark:brightness-[0.2] dark:grayscale'
-                width={24}
-                height={24}
-              />
-            </div>
-            Morimitsu | Tradição Jiu-jitsu
-          </Link>
-        </div> */}
         <div className='flex flex-1 items-center justify-center'>
           <div className='w-full max-w-md'>
             <div className='w-full lg:hidden flex items-center justify-center mb-6'>
               <Image
                 src='/logo_morimtsu.png'
-                alt='Image'
+                alt='Morimtisu Logo'
                 className='h-40 w-40 object-cover dark:brightness-[0.2] dark:grayscale'
-                width={200}
-                height={200}
+                width={160}
+                height={160}
               />
             </div>
             <LoginForm />
--- src/components/forms/login-form.tsx
+++ src/components/forms/login-form.tsx
@@ -3,46 +3,51 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import Link from 'next/link'
+import { PasswordInput } from '../ui/password-input'
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<'form'>) {
   return (
-    <form className={cn('flex flex-col gap-6 p-6', className)} {...props}>
-      <div className='flex flex-col items-center gap-2'>
-        <h1 className='text-2xl font-bold text-left'>Entre na sua conta</h1>
-        <p className='text-muted-foreground text-sm text-balance'>
+    <form className={cn('flex flex-col gap-6 p-5', className)} {...props}>
+      <div className='flex flex-col items-center gap-1'>
+        <h1 className='font-poppins text-2xl font-semibold text-left'>
+          Entre na sua conta
+        </h1>
+        <p className='font-poppins text-muted-foreground text-balance'>
           Insira seu e-mail abaixo para acessar sua conta.
         </p>
       </div>
       <div className='grid gap-6'>
-        <div className='grid gap-3'>
-          <Label htmlFor='email'>Email</Label>
+        <div className='grid gap-1'>
+          <Label htmlFor='email' className='font-semibold font-poppins'>
+            Email
+          </Label>
           <Input id='email' type='email' placeholder='m@example.com' required />
         </div>
-        <div className='grid gap-3'>
+        <div className='grid gap-1'>
           <div className='flex items-center'>
-            <Label htmlFor='password'>Senha</Label>
+            <Label htmlFor='password' className='font-semibold font-poppins'>
+              Senha
+            </Label>
             <Link
               href='/forgot-password/send-email'
-              className='ml-auto text-sm underline-offset-4 hover:underline'
+              className='ml-auto text-sm underline-offset-2 underline font-poppins'
             >
-              Esqueceu sua senha?
+              Esqueci a senha
             </Link>
           </div>
-          <Input id='password' type='password' required />
+          <PasswordInput
+            id='password'
+            required
+            placeholder='Digite sua senha'
+          />
         </div>
-        <Button type='submit'  className='w-full cursor-pointer'>
+        <Button type='submit' className='w-full cursor-pointer'>
           Entrar
         </Button>
       </div>
-      {/* <div className="text-center text-sm">
-        Ainda não tem uma conta?{" "}
-        <a href="#" className="underline underline-offset-4">
-          Cadastre-se
-        </a>
-      </div> */}
     </form>
   )
 }

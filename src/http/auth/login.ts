e6c172e1916252b496fe0814dacb06c8489a06f4
Ismael <ismael.henrique.dev@gmail.com>
2025-10-20 22:08:15 -0300

    refactor(login request): add LoginResponse type and user role definitions; update login function to use typed response

--- src/http/auth/login.ts
+++ src/http/auth/login.ts
@@ -4,15 +4,22 @@ import { cookies } from 'next/headers'
 import { api } from '@/services/api'
 import { getAxiosStatusCode } from '@/lib/utils'
 import { LoginFormData } from '@/validators/login'
+import { LoginResponse } from '@/lib/definitions'
 
 export async function login(formData: LoginFormData) {
   try {
-    const { data } = await api.post('/auth/login', formData)
+    const { data: response } = await api.post<LoginResponse>(
+      '/auth/login',
+      formData
+    )
+
+    const expiresAt = new Date(Date.now() + 60 * 60 * 1000)
 
     const cookieStore = await cookies()
-    cookieStore.set('accessToken', data.token, {
+
+    cookieStore.set('accessToken', response.token, {
       path: '/',
-      expires: new Date(Date.now() + 60 * 60 * 1000),
+      expires: expiresAt,
     })
 
     return { message: 'Login realizado com sucesso.', status: 'success' }
